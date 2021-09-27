c82a83e809501b06d0edd0d495bbe15186d6eaf9
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-09-27 08:18:16 +0530

    Implement priority queue using linked lists

---
+++ DataStructures/001_PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_PriorityQueue
+{
+    public class PriorityQueue<T>
+    {
+        Node<T> Head;
+        public int Count { get; private set; }
+        public void Enqueue(T newVal, int priority)
+        {
+            Count++;
+            var newN = new Node<T>();
+            newN.Value = newVal;
+            newN.Priority = priority;
+            if (Head == null)
+            {
+                Head = newN;
+            }
+            else if (Head.Priority < newN.Priority)
+            {
+                newN.Next = Head;
+                Head = newN;
+            }
+            else
+            {
+                var curr = Head;
+                while (curr != null)
+                {
+                    if (curr.Next == null || curr.Next.Priority < newN.Priority)
+                    {
+                        newN.Next = curr.Next;
+                        curr.Next = newN;
+                        break;
+                    }
+                    curr = curr.Next;
+                }
+            }
+        }
+
+        public T Dequeue()
+        {
+            var val = Head.Value;
+            Head = Head.Next;
+            Count--;
+            return val;
+        }
+
+        public T Peek()
+        {
+            return Head.Value;
+        }
+    }
+    public class Node<T>
+    {
+        public T Value;
+        public int Priority;
+        public Node<T> Next;
+    }
+}
---
+++ DataStructures/001_PriorityQueue/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_PriorityQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * In this session, we will implement priority queue using linked lists.
+             * When using the linked list, the highest priority node will be on top(head) of the linked list 
+             * and all the other elements are arranged in sorting orders.
+             */
+            Demo();
+            //Time Complexity
+            // Enqueue  - O(n)
+            // Dequeue  - O(1)
+            // Peek     - O(1)
+        }
+        public static void Demo()
+        {
+            var q = new PriorityQueue<int>();
+            q.Enqueue(5, 1);
+            q.Enqueue(6, 1);
+            q.Enqueue(7, 2);
+            q.Enqueue(8, 1);
+            q.Enqueue(9, 1);
+            q.Enqueue(10, 3);
+            Console.WriteLine("Peek : " + q.Peek());
+            while (q.Count > 0)
+            {
+                var n = q.Dequeue();
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
