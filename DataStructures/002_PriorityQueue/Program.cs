dd5ce503fed0156624d4971908d171a8f8eb619c
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-09-27 08:49:58 +0530

    Implement priority queue using heap

---
+++ DataStructures/002_PriorityQueue/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_PriorityQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo();
+            //Time Complexity
+            // Enqueue  - O(Log n)
+            // Dequeue  - O(Log n)
+            // Peek     - O(1)
+        }
+        public static void Demo()
+        {
+            var q = new PriorityQueue<int>();
+            q.Enqueue(5, 1);
+            q.Enqueue(6, 1);
+            q.Enqueue(7, 2);
+            q.Enqueue(8, 1);
+            q.Enqueue(9, 1);
+            q.Enqueue(10, 3);
+            Console.WriteLine("Peek : " + q.Peek());
+            while (q.Count > 0)
+            {
+                var n = q.Dequeue();
+                Console.WriteLine(n);
+            }
+        }
+    }
+
+}
