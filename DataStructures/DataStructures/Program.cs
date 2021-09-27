658fc51adb9925b50ddef53b1dfc77216d295138
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-09-27 07:59:17 +0530

    creating the soution

---
+++ DataStructures/DataStructures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * This solution will contain the data structures that I learned.
+             */
+        }
+    }
+}
