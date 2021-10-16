726b62d5b954fae7a82003f9b6bafde5c812d06d
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-10-16 13:43:29 +0530

    word concat using trie data structure

---
+++ DataStructures/001_ConcatenatedWords/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_ConcatenatedWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sln = new Solution();
+            var words = new string[] { "cat", "cats", "catsdogcats", "dog", "dogcatsdog", "hippopotamuses", "rat", "ratcatdogcat"};
+            var ans = sln.FindAllConcatenatedWordsInADict(words);
+            foreach (var item in ans)
+            {
+                Console.Write(item+", ");
+            }
+            Console.WriteLine();
+        }
+        public class Solution
+        {
+            public class Trie
+            {
+                public Dictionary<char, Trie> dict = new Dictionary<char, Trie>();
+                public bool isWord;
+                public void AddWord(string s, int ind)
+                {
+                    if (ind == s.Length)
+                    {
+                        isWord = true;
+                        return;
+                    }
+                    var ch = s[ind];
+                    if (!dict.ContainsKey(ch))
+                    {
+                        dict.Add(ch, new Trie());
+                    }
+
+                    dict[ch].AddWord(s, ind+1);
+                }
+            }
+            IList<string> ans = new List<string>();
+            Trie root = new Trie();
+            public IList<string> FindAllConcatenatedWordsInADict(string[] words)
+            {
+                foreach (var word in words)
+                {
+                    root.AddWord(word, 0);
+                }
+
+                foreach (var word in words)
+                {
+                    if (Dfs(root, word, 0))
+                    {
+                        ans.Add(word);
+                    }
+                }
+                return ans;
+            }
+            public bool Dfs(Trie node, string word, int ind, bool isFirstWordFound = false)
+            {
+                if (word.Length == ind)
+                {
+                    return false;
+                }
+                var ch = word[ind];
+                if (node.dict.ContainsKey(ch))
+                {
+                    var newNode = node.dict[ch];
+                    if (newNode.isWord)
+                    {
+                        if (ind == word.Length -1)
+                        {
+                            return isFirstWordFound;
+                        }
+                        if (Dfs(root, word, ind+1, true))
+                        {
+                            return true;
+                        }
+                    }
+                    if (ind == word.Length - 1)
+                    {
+                        return false;
+                    }
+                    return Dfs(newNode, word, ind+1, isFirstWordFound);
+                }
+                return false;
+            }
+        }
+    }
+}
