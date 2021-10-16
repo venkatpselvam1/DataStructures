c205907057657a587e36492ea89f2ee5d230f928
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-10-16 16:09:37 +0530

    DFS with adjacency matrix

---
+++ DataStructures/003_DepthFirstSearch/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_DepthFirstSearch
+{
+    class Program
+    {
+        public class Graph
+        {
+            bool[] rec;
+            int[,] graph;
+            public Graph(int n)
+            {
+                this.graph = new int[n, n];
+                this.rec = new bool[n];
+            }
+            public void AddEdge(int a, int b)
+            {
+                graph[a, b] = 1;
+                graph[b, a] = 1;
+            }
+            public void DeleteEdge(int a, int b)
+            {
+                graph[a, b] = 0;
+                graph[b, a] = 0;
+            }
+
+            public void Print()
+            {
+                for (int i = 0; i < graph.GetLength(0); i++)
+                {
+                    for (int j = 0; j < graph.GetLength(1); j++)
+                    {
+                        Console.Write(graph[i, j] + ", ");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+            public void Dfs(int n)
+            {
+                rec[n] = true;
+                Console.WriteLine(n);
+                for(int i = 0; i < graph.GetLength(0); i++)
+                {
+                    if (graph[n, i] == 1 && !rec[i])
+                    {
+                        rec[i] = true;
+                        Dfs(i);
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var graph = new Graph(5);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(2, 1);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(4, 3);
+            graph.AddEdge(4, 0);
+            graph.AddEdge(4, 1);
+            graph.AddEdge(3, 1);
+            graph.Dfs(2);
+        }
+    }
+}
