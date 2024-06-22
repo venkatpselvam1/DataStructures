89e57b45b1dae40989b0493cb34fab87aed04065
Venkatesan Panneerselvam <venkat.selvam@agoda.com>
2024-06-22 12:23:24 +0700

    Migrating PriorityQueue

---
+++ DataStructuresCore/DataStructures/001_PriorityQueue/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using _001_PriorityQueue;
+
+var q = new VenkatPriorityQueue<int>();
+q.Enqueue(5, 1);
+q.Enqueue(6, 1);
+q.Enqueue(7, 2);
+q.Enqueue(8, 1);
+q.Enqueue(9, 1);
+q.Enqueue(10, 3);
+Console.WriteLine("Peek : " + q.Peek());
+while (q.Count > 0)
+{
+    var n = q.Dequeue();
+    Console.WriteLine(n);
+}
